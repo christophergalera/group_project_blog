e5b34e57989a6d96c17b4dcd05642635140e0593
InoAlexander <inomoreno91@gmail.com>
2021-06-27 01:04:40 -0700

    description

--- client/src/components/PostDetails.js
+++ client/src/components/PostDetails.js
@@ -1,3 +1,6 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import {navigate, Link} from '@reach/router';
+
+// this page is ONLY NEEDED if you want to view the post or content in a different "page" it will display different from the home page and give access to options like delete and edit with the option to go back as well. 
+
