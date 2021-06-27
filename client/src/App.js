61f37179c7af5931add979eb7acd9cf5369b3179
InoAlexander <inomoreno91@gmail.com>
2021-06-27 03:14:56 -0700

    added routes to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { Router } from '@reach/router';
 import { Link, navigate } from '@reach/router';
 import AllPosts from './components/AllPosts';
 import NewPost from './components/NewPost';
+import EditPosts from './components/EditPosts';
+import BlogDetails from './components/BlogDetails';
 import Login from './components/Login';
 import RegisterUser from './components/register';
 import Forbidden from './views/Forbidden';
@@ -14,6 +16,8 @@ function App() {
       <Router>
         <AllPosts path="/blog/all_posts" /> 
         <NewPost path="/blog/new_post" />
+        <BlogDetails path='/blog/:post_id'/>
+        <EditPosts path='/blog/:id/edit'/>
         <Login path="/login" />
         <RegisterUser path="/register" />
         <Forbidden path="/forbidden_page" />
